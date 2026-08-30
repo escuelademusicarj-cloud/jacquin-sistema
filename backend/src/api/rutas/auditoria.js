261f49ed806ac2627c636be4d05f42a001b0a212
escuelademusicarj-cloud <escuelademusicarj@gmail.com>
2026-08-30 12:09:15 -0500

    Create auditoria.js

---
+++ backend/src/api/rutas/auditoria.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { listarAuditoria } from "../../auditoria/servicio.js";
+import { respuestaExitosa } from "../middlewares/manejoErrores.js";
+import { requiereAutenticacion } from "../middlewares/autenticacion.js";
+import { requierePermiso } from "../middlewares/autorizacion.js";
+
+export const rutasAuditoria = Router();
+rutasAuditoria.use(requiereAutenticacion);
+
+// Solo Administración y Dirección (permiso auditoria:ver, ver SQL de siembra).
+rutasAuditoria.get("/", requierePermiso("auditoria:ver"), async (req, res, next) => {
+  try {
+    const filas = await listarAuditoria({
+      rol: req.query.rol || undefined,
+      accion: req.query.accion || undefined,
+      desde: req.query.desde || undefined,
+      hasta: req.query.hasta || undefined,
+    });
+    respuestaExitosa(res, filas);
+  } catch (err) { next(err); }
+});
